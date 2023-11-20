e969030e95d33cc60406348d16a4b20b254a2f32
artjombokov <abokov@tktk.ee~>
2023-11-20 12:04:58 +0200

    week3

---
+++ server.js
@@ -0,0 +1,45 @@
+let friends = [
+    {
+        firstName: 'Harry',
+        age: 11,
+        isWizard: true,
+        friends: ['Ron', 'Hermione', 'Hedwig'],
+        introduce: function() {
+            console.log(`Greetings, my name is ${this.firstName}.`);
+        }
+    },
+    {
+        firstName: 'Ron',
+        age: 11,
+        isWizard: true,
+        friends: ['Harry', 'Hermione', 'Hedwig'],
+        introduce: function() {
+            console.log(`Greetings, my name is ${this.firstName}.`);
+        }
+    },
+    {
+        firstName: 'Hedwig',
+        age: 'unknown',
+        isWizard: true,
+        friends: ['Harry', 'Ron', 'Hermione'],
+        introduce: function() {
+            console.log(`${this.firstName} says hoot-hoot.`);
+           } 
+    }                 
+]
+//filter()
+let students = friends.filter(friend => {
+    if(friend.isWizard) {
+        return friend;
+    }
+});
+
+//map()
+let studentsList = students.map(student => {
+    return student.firstName;
+});
+
+//sort()
+studentsList.sort((a, b) => {return b.localeCompare(a)});
+
+console.log(studentsList);
